746f80154907a6187e4f1cf858d8b5285ff3f969
PhilipK13 <67237885+PhilipK13@users.noreply.github.com>
2021-12-06 11:54:49 -0500

    Added firebase config

--- config/firebase.ts
+++ config/firebase.ts
@@ -1,5 +1,10 @@
 const devConfig = {
-   
+    apiKey: "AIzaSyB_JrJtg4oJr8hXu7b69zHexNtpIz5Cx1M",
+    authDomain: "moneymanager-28b49.firebaseapp.com",
+    projectId: "moneymanager-28b49",
+    storageBucket: "moneymanager-28b49.appspot.com",
+    messagingSenderId: "651264744261",
+    appId: "1:651264744261:web:dfc8720bd6f71c00dffc2c"
 };
 
 const prodConfig = {
