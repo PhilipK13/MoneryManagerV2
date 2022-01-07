0d4268c86f384dcc19b2a8d260bc1ca6694d52ec
PhilipK13 <67237885+PhilipK13@users.noreply.github.com>
2022-01-07 11:20:40 -0500

    added production mode for firebase

--- config/firebase.ts
+++ config/firebase.ts
@@ -8,7 +8,12 @@ const devConfig = {
 };
 
 const prodConfig = {
-
+    apiKey: "AIzaSyB_JrJtg4oJr8hXu7b69zHexNtpIz5Cx1M",
+    authDomain: "moneymanager-28b49.firebaseapp.com",
+    projectId: "moneymanager-28b49",
+    storageBucket: "moneymanager-28b49.appspot.com",
+    messagingSenderId: "651264744261",
+    appId: "1:651264744261:web:dfc8720bd6f71c00dffc2c"
 };
 
 export default process.env.NODE_ENV === "production" ? prodConfig : devConfig;
